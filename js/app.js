787828f9c9fccf410c9c34fa79f8fea3fb05fd76
Natalia Garrido <natgramirez@outlook.com>
2017-05-30 12:26:16 -0400

    agregando reconocimiento de imagen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+// Array.from para que reconozca la clase como un arreglo
+
+var fotos = Array.from(document.getElementsByClassName('col-4'));
+
+fotos.forEach(function(foto) {
+    foto.addEventListener('click', test);
+});
+
+
+
+
+function test() {
+    alert('ahdjasd');
+}
