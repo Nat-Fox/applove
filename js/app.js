5291df14f1cb09c2d87aff03851984dda6da64db
Natalia Garrido <natgramirez@outlook.com>
2017-05-31 19:03:03 -0400

    agregando modales a la galeria

--- js/app.js
+++ js/app.js
@@ -2,17 +2,55 @@
  * Archivo principal de funcionalidad de JS
  */
 
-// Array.from para que reconozca la clase como un arreglo
 
-var fotos = Array.from(document.getElementsByClassName('col-4'));
 
-fotos.forEach(function(foto) {
-    foto.addEventListener('click', test);
-});
+// Función que se invoca así misma
+(function modalesPorFoto() {
+    // Array.from para que reconozca la clase como un arreglo
+    var fotos = Array.from(document.getElementsByClassName('img-galery'));
+    // Id perteneciente al div donde ubicaremos el modal
+    var modalFoto = document.getElementById('img-galery-modal');
+    var modalGaleria, close, img;
+    fotos.forEach(function(foto) {
+        foto.addEventListener('click', function() {
+            // Borro todo lo que esta dentro del modal
+            modalFoto.innerHTML = '';
+            // En modalGaleria crearemos el div que contendra al modal
+            modalGaleria = document.createElement('div');
+            // Le creamos la clase modal-galeria donde incluiremos la foto, y sus estilos en main.css
+            modalGaleria.classList.add('modal-galeria');
+            modalGaleria.innerHTML = foto.innerHTML;
 
-function test() {
-    alert('ahdjasd');
-}
+            //Hacemos hide el modal para antes del click
+            modalFoto.appendChild(modalGaleria);
+            modalFoto.classList.remove('hide');
+
+            // La imagen x para cerrar la foto ira en otro div
+            close = document.createElement('div');
+            // En la clase close se tratan los estilos para la x
+            close.classList.add('close');
+
+            // Se agrega la imagen a img con src
+            img = document.createElement('img');
+            img.setAttribute('src', 'https://www.iconfinder.com/data/icons/geomicons/32/672366-x-128.png');
+
+            // Se apenda la img de x a close y esto al modal foto
+            close.appendChild(img);
+            modalFoto.appendChild(close);
+
+            // Cuando se haga click en close se escondera el modal nuevamente
+            close.addEventListener('click', function() {
+                modalFoto.classList.add('hide');
+            });
+
+            var close = document.querySelector('#img-galery-modal .close');
+            close.addEventListener('click', function() {
+                modalFoto.classList.add('hide');
+            });
+        });
+    });
+})();
+
 
 /* Ejemplo blanca sobre modales */
 (function main() {
